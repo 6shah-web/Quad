7c81d68d7da66c6570ddbfcb2e666d5bf08a00ad
Your Name <you@example.com>
2022-08-13 14:15:20 +0100

    Quad A rectangle

--- QuadA.go
+++ QuadA.go
@@ -1,21 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"github.com/01-edu/z01"
+)
 
 func main() {
+	width := 1
 	height := 5
-	width := 20
 
-	for h := 1; h <= height; h++ {
-		for w := 1; w <= width; w++ {
-			if h == 1 || h == height {
-				fmt.Print("*")
-			} else if w == 1 || w == width {
-				fmt.Print("*")
-			} else {
-				fmt.Print(" ")
+	// Header rectangle
+	if width > 1 {
+		z01.PrintRune('o')
+	}
+	for i := 1; i < width-1; i++ {
+		z01.PrintRune('-')
+	}
+	z01.PrintRune('o')
+	z01.PrintRune('\n')
+
+	// body rectangle and side
+	for k := 1; k < height-1; k++ {
+		if height > 1 {
+			z01.PrintRune('|')
+			for j := 1; j < width-1; j++ {
+				z01.PrintRune(' ')
+			}
+			if width > 1 {
+				z01.PrintRune('|')
 			}
 		}
-		fmt.Println()
+		z01.PrintRune('\n')
+	}
+
+	// footer rectangle
+	if height > 1 {
+		if width > 1 {
+			z01.PrintRune('o')
+		}
+		for i := 1; i < width-1; i++ {
+			z01.PrintRune('-')
+		}
+
+		z01.PrintRune('o')
+		z01.PrintRune('\n')
+	} else {
+		return
 	}
 }
