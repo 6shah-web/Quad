248f9311bf9962a889fd1251945b5e5875250afb
fmoceri <moceri.flavio@gmail.com>
2022-08-14 09:46:04 +0100

    final-quad-project

---
+++ quadA.go
@@ -0,0 +1,28 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func QuadA(x, y int) {
+	for h := 1; h <= y; h++ {
+		for w := 1; w <= x; w++ {
+			if (h == 1 || h == y) && (w == 1 || w == x) {
+				if h == 1 && w == 1 {
+					z01.PrintRune('o')
+				} else if h == 1 && w == x {
+					z01.PrintRune('o')
+				} else if h == y && w == 1 {
+					z01.PrintRune('o')
+				} else if h == y && w == x {
+					z01.PrintRune('o')
+				}
+			} else if (h == 1 && w > 1 && w < x) || (h == y && w > 1 && w < x) {
+				z01.PrintRune('-')
+			} else if w == 1 || w == x {
+				z01.PrintRune('|')
+			} else {
+				z01.PrintRune(' ')
+			}
+		}
+		z01.PrintRune('\n')
+	}
+}
